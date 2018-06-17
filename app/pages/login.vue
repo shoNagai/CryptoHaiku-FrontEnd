a3439ea1e5c13409cc16dcfe906084cd2eb1928b
shonagai <strawberry4062@gmail.com>
2018-06-18 00:20:58 +0900

    add uPort-connect

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -10,15 +10,16 @@
       <h2 class="subtitle">
         CryptoHaiku is a new form of Japanese culture Haiku using blockchains
       </h2>
-      <a @click="doLogin">
-        <img :src="image_src">
-      </a>
+      <button class="button uport-connect-btn" @click="doConnectUPort">
+        Connect with uPort
+      </button>
     </div>
   </div>
 </template>
 
 <script>
 import firebase from '~/plugins/firebase'
+import { Connect, SimpleSigner } from 'uport-connect'
 import { mapActions } from 'vuex'
 export default {
   layout (context) {
@@ -36,20 +37,46 @@ export default {
       this.login()
         .then(() => console.log('resloved'))
         .catch((err) => console.log(err))
+    },
+    doConnectUPort() {
+      const uport = new Connect('CryptoHaiku', {
+        clientId: '2oiX15UxxGBJpa1zYrG4TU1iqhd5YJUkPZG',
+        network: 'rinkeby',
+        signer: SimpleSigner('643aab8b75b2622a363f4c607b4b7f5f7cd4b0121432094af0e7ce9f80d6dd88')
+      })
+
+      // Request credentials to login
+      uport.requestCredentials({
+        requested: ['name', 'phone', 'country'],
+        notifications: true // We want this if we want to recieve credentials
+      })
+      .then((credentials) => {
+        // Do something
+        console.log(credentials)
+      })
     }
+
   },
   async mounted () {
-    let user = await new Promise((resolve, reject) => {
-      firebase.auth().onAuthStateChanged((user) =>  {
-        resolve(user)
-        this.setUser(user)
-        if (user) {
-          this.$router.push('/')
-        }else {
-          this.isLoading = false
-        }
-      })
-    })
+    this.isLoading = false
+    // let user = await new Promise((resolve, reject) => {
+    //   firebase.auth().onAuthStateChanged((user) =>  {
+    //     resolve(user)
+    //     this.setUser(user)
+    //     if (user) {
+    //       this.$router.push('/')
+    //     }else {
+    //       this.isLoading = false
+    //     }
+    //   })
+    // })
   }
 }
 </script>
+
+<style>
+.uport-connect-btn {
+  background: #7958d8;
+  color: #f9f9fa;
+}
+</style>
